a5036b891bc9800276d1ead36a72be6382c340ab
BobbyMbata <mbataemmanuelbobby@gmail.com>
2022-10-20 04:46:37 -0700

    tassk1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main- print_alphabet
+ *
+ * Return: always 0
+ */
+void print_alphabet(void)
+
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'
+		_putchar(letter);
+
+		_putchar('\n');
+	return (0);
+}
